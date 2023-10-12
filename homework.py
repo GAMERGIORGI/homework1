00b7e17111c93e9d639100575c3f4e40bfe2d9cc
George Murachashvili <gamergiogi339@gmail.com>
2023-10-12 19:43:30 +0400

    first commit

---
+++ homework.py
@@ -0,0 +1,40 @@
+user=("hola")
+      
+
+      print("hello
+            
+
+            user=("sad
+                  
+
+                  user=("fever")
+
+
+                  print("rrr"
+                        angry face=("
+                              
+            user=("23)
+                  3r32
+                  print=(correct
+                         user=(hello'
+                               print("home")
+                               (5 + 1
+                                print= (hell0
+                                        user=(56+34*2
+                                              
+
+                                        
+print(234+432
+      user=str(input
+               print(hello
+                     user=(45-23
+                           user=(655-67
+                                 print(apple
+                                       user=str(inpt(
+user=str(input()"enter your age")
+         print("getting older")
+         user=("python")
+user=str(input()"calculator")
+print("javascript")
+print("http")
+print("GOA")
